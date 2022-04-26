22ab5bc1753cb754c1b137a8bdf0d3b569f4b7be
Yunus Emre <yunus_kole2323@outlook.com>
2022-04-26 16:40:04 +0300

    Added Base Permissions

--- django/blog_api/tests.py
+++ django/blog_api/tests.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from blog.models import Post, Category
 from django.contrib.auth.models import User
+from rest_framework.test import APIClient
 
 
 class PostTests(APITestCase):
@@ -25,3 +26,29 @@ class PostTests(APITestCase):
         url = reverse('blog_api:listcreate')
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+    
+    def test_post_update(self):
+        
+        client = APIClient()
+        
+        self.test_category = Category.objects.create(name='django')
+        self.test_user1 = User.objects.create(username='test_user1', password='123456789')
+        self.test_user2 = User.objects.create(username='test_user2', password='123456789')
+        test_post = Post.objects.create(
+            category_id=1, title='Post Title', excerpt='Post Excerpt', content='Post Content', slug='post-title', author_id=1, status='published'
+        )
+        
+        client.login(username=self.test_user1.username, password='123456789')
+        
+        url = reverse(('blog_api:detailcreate'), kwargs={'pk': 1})
+        
+        response = client.put(url, {
+            "title": "New",
+            "author": 1,
+            "excerpt": "New",
+            "content": "New",
+            "status": "published"
+        }, format='json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
