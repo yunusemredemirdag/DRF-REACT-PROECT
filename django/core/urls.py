1223d11fed62de81a27bb283f85c961cd0240bac
Yunus Emre <yunus_kole2323@outlook.com>
2022-04-28 10:49:59 +0300

    API Schema and Documentation Generation

--- django/core/urls.py
+++ django/core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -10,5 +12,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    #path('', include('blog.urls', namespace='blog')),
+    path('docs/', include_docs_urls(title="BlogAPI")),
+    path('schema', get_schema_view(
+        title="BlogAPI",
+        description="API for the BlogAPI",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
